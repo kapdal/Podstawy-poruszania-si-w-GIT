23b1f36d365022b4fe89a72967134c2be5b22bb4
Jakub Obloza <jakub.obloza@studenci.collegiumwitelona.pl>
2026-06-28 04:47:13 +0200

    dodalem logs

--- skrypt.sh
+++ skrypt.sh
@@ -2,3 +2,12 @@
 if [[ "$1" == "--date" ]]; then
 	date
 if
+
+if [[ "$1" == "--logs" && -z "$2" ]]; then
+    for i in {1..100}; do
+        filename="log$i.txt"
+        echo "Nazwa pliku: $filename" > "$filename"
+        echo "Skrypt: $0" >> "$filename"
+        echo "Data: $(date)" >> "$filename"
+    done
+fi
